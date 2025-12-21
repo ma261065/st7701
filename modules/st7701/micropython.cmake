61bec7de8b99f48bff65939027954eb3f9a15d99
Mike Alexander <ma261065@users.noreply.github.com>
2025-12-22 10:48:10 +1100

    Fix target_sources for usermod_st7701

--- modules/st7701/micropython.cmake
+++ modules/st7701/micropython.cmake
@@ -1,6 +1,6 @@
 add_library(usermod_st7701 INTERFACE)
 
-target_sources(usermod_st7701s INTERFACE
+target_sources(usermod_st7701 INTERFACE
     ${CMAKE_CURRENT_LIST_DIR}/st7701.c)
 
 target_include_directories(usermod_st7701 INTERFACE
