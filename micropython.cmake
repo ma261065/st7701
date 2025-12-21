efddb5028b3a2946840ecd4f9eb720b1b5f20e65
Mike Alexander <ma261065@users.noreply.github.com>
2025-12-22 10:45:22 +1100

    Delete micropython.cmake

--- micropython.cmake
+++
@@ -1,5 +0,0 @@
-# This top-level micropython.cmake is responsible for listing
-# the individual modules we want to include.
-# Paths are absolute, and ${CMAKE_CURRENT_LIST_DIR} can be
-# used to prefix subdirectories.
-include(${CMAKE_CURRENT_LIST_DIR}/st7701/micropython.cmake)
