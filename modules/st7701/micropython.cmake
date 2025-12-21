25f83bcd1c677c0effe5adccdbb9102c3b66bc17
Mike Alexander <ma261065@users.noreply.github.com>
2025-12-22 10:42:19 +1100

    Initial commit

---
+++ modules/st7701/micropython.cmake
@@ -0,0 +1,12 @@
+add_library(usermod_st7701 INTERFACE)
+
+target_sources(usermod_st7701s INTERFACE
+    ${CMAKE_CURRENT_LIST_DIR}/st7701.c)
+
+target_include_directories(usermod_st7701 INTERFACE
+    ${CMAKE_CURRENT_LIST_DIR}
+    $ENV{IDF_PATH}/components/esp_lcd/include
+    $ENV{IDF_PATH}/components/esp_lcd/rgb/include
+)
+
+target_link_libraries(usermod INTERFACE usermod_st7701)
